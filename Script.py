3743ec826898f3b96679573b92ebef3a4ca051a0
vloggerdeven <98028377+vloggerdeven@users.noreply.github.com>
2022-02-19 06:30:18 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -95,7 +95,7 @@ This module only works for my admins
 ★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
 ★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
 ★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
-     LOG_TEXT_G = #NewGroup
+     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
 Added By - {}
