cb5858618a2cdcb44d684102884bbe5d0f44623c
DK BOTx TG <100774327+DKBOTx@users.noreply.github.com>
2022-03-17 22:29:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/DKBOTx/private-bot.git /private-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /private-bot
 fi
-cd /EvaMaria
+cd /private-bot
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "sᴛᴀʀᴛɪɴɢ 𝗗𝗞 𝗕𝗢𝗧𝘅........"
 python3 bot.py
