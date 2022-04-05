f8f0d7939010cab6d31d0ae663bac9a9a50e12f8
ɢᴛ ᠰ ʜᴇʀᴏ <92996711+TGGTHERO@users.noreply.github.com>
2022-04-05 13:29:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -41,7 +41,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("Request Your Own 🙂", show_alert=True)
+        return await query.answer("{query.from_user.first_name} മോനെ ഇത് നിനക്കുലതല്ല 🤭\n\n{query.message.reply_to_message.from_user.first_name} ന്റെ റിക്വസ്റ്റ് ആണ് ഇത് 🙂\n\nRequest your own 🥰\n\n© MOVIE HUB", show_alert=True)
     try:
         offset = int(offset)
     except:
