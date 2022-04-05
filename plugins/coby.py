3849fa1a069788c03628881ee955f8ca2c894adf
Ssfive123 <91235488+Ssfive123@users.noreply.github.com>
2022-04-05 11:42:05 +0530

    Delete coby.py

--- plugins/coby.py
+++
@@ -1,5 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command('source'))
-async def source(bot, update):
-    await update.reply_photo('https://telegra.ph/file/e175384031bd8081ffd2b.jpg')
