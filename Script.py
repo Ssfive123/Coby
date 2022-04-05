0ce8ec98aec2a2cf4f03d9e924fda50246fe7f20
Ssfive123 <91235488+Ssfive123@users.noreply.github.com>
2022-04-05 10:45:36 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -103,7 +103,7 @@ Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
 Added By - {}
 """
-    LOG_TEXT_P = """#NewUser
+    LOG_TEXT_P = """📮 ᴄᴏʙʏ ʙᴏᴛ #NewUser
 ID - <code>{}</code>
 Name - {}
 """
