c2db2a0850af92dc2c5bf47e4ed6797ddb038be8
Ssfive123 <91235488+Ssfive123@users.noreply.github.com>
2022-04-05 12:23:51 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -54,8 +54,6 @@ async def showid(client, message):
             quote=True
         )
 
-from pyrogram import Client, filters
-
 @Client.on_message(filters.command('source'))
 async def source(bot, update):
     await update.reply_photo('https://telegra.ph/file/e175384031bd8081ffd2b.jpg')
