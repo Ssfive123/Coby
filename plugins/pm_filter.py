a994318426a58e635660cb47366eb4905cb6801c
DK BOTx TG <100774327+DKBOTx@users.noreply.github.com>
2022-04-27 15:23:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -42,7 +42,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("{query.from_user.first_name} മോനെ ഇത് നിനക്കുലതല്ല 🤭\n\n{query.message.reply_to_message.from_user.first_name} ന്റെ റിക്വസ്റ്റ് ആണ് ഇത് 🙂\n\nRequest your own 🥰\n\n© MOVIE HUB", show_alert=True)
+        return await query.answer(UNAUTHORIZED_CALLBACK_TEXT, show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -61,6 +61,8 @@ async def next_page(bot, query):
     if not files:
         return
     settings = await get_settings(query.message.chat.id)
+    pre = 'Chat' if settings['redirect_to'] == 'Chat' else 'files'
+
     if settings['button']:
         btn = [
             [
