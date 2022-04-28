8a2d2e426b9878547994b634e2236917ef56e3c3
Shivaprasad <91235488+Ssfive123@users.noreply.github.com>
2022-04-28 13:30:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -771,9 +771,9 @@ async def auto_filter(client, msg, spoll=False):
                                                         
                         ]
                     )
-                )
-                await asyncio.sleep(3)
-                await k.delete()
+                 )
+                 await asyncio.sleep(3)
+                 await k.delete()
   
     if spoll:
         await msg.message.delete()
