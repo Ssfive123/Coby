cfb5e2d7ed469a949834327541cb04a60d332bb3
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-04-28 21:46:13 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ʜᴇʟᴘ', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇs', url='https://t.me/MovieHubOtt')
+            InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇs', url='https://t.me/SS_Linkz')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
