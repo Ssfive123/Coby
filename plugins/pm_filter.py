02dc0dda1a882f43491250713b2411ec387f5c3e
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-04-28 23:03:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -65,22 +65,22 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"📂 [{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}#{query.from_user.id}'
-                )
-            ] 
+                    text=f"▫️ {get_size(file.file_size)} ‣ {file.file_name}", callback_data=f'files#{file.file_id}'
+                ),
+            ]
             for file in files
         ]
     else:
-        btn = [        
+        btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}", callback_data=f'{pre}#{file.file_id}#{query.from_user.id}'
+                    text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
-                    callback_data=f'{pre}_#{file.file_id}#{query.from_user.id}',
-                )
-            ] 
+                    callback_data=f'files_#{file.file_id}',
+                ),
+            ]
             for file in files
         ]
 
