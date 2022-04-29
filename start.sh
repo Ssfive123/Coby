d705ca9a7ed7c96c4622a25d5b7d1d6c9de4e0db
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-04-29 08:24:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/4kcinemas/Netflix-Bot /Netflix-Bot
+  git clone https://github.com/4kcinemas/Netflix-Bot.git /Netflix-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Netflix-Bot
