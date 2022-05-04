d3d9cf25eac87eb70cf65d4efed92f5877850030
Shivaprasad <91235488+Ssfive123@users.noreply.github.com>
2022-05-04 19:27:21 +0530

    Update Stats.py

--- plugins/AVR/Stats.py
+++ plugins/AVR/Stats.py
@@ -21,4 +21,4 @@ async def ping(_, message):
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
     uptime = time.strftime("%Hh | %Mm | %Ss", time.gmtime(time.time() - BOT_START_TIME))   
-    await avr.edit(f"-𝙲𝚄𝚁𝚁𝙴𝙽𝚃 𝙱𝙾𝚃 𝚂𝚃𝙰𝚃𝚄𝚂-\n\n‹› 𝙿𝙾𝙽𝙶! : {time_taken_s:.3f} ms\n‹› 𝙱𝙾𝚃 𝚄𝙿𝚃𝙸𝙼𝙴 : {uptime}")
+    await avr.edit(f"-ᴄᴜʀʀᴇɴᴛ ʙᴏᴛ sᴛᴀᴛᴜs-\n\n‹› 🏓 ᴘᴏɴɢ : {time_taken_s:.3f} ms\n‹› ⏰ ᴜᴘᴛɪᴍᴇ : {uptime}")
