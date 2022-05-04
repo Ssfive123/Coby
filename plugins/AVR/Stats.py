12c6bb0a8948a3e04a6847405b62f173b077af3a
Shivaprasad <91235488+Ssfive123@users.noreply.github.com>
2022-05-04 22:15:46 +0530

    Update Stats.py

--- plugins/AVR/Stats.py
+++ plugins/AVR/Stats.py
@@ -7,6 +7,7 @@ import time
 import heroku3
 import requests
 from pyrogram import Client, filters
+from info import DELETE_TIME
 
 #=====================================================
 BOT_START_TIME = time.time()
@@ -19,6 +20,6 @@ async def ping(_, message):
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
     uptime = time.strftime("%Hh | %Mm | %Ss", time.gmtime(time.time() - BOT_START_TIME))   
-    await avr.edit(f"-ᴄᴜʀʀᴇɴᴛ ʙᴏᴛ sᴛᴀᴛᴜs-\n\n‹› 🏓 ᴘᴏɴɢ : {time_taken_s:.3f} ms\n‹› ⏰ ᴜᴘᴛɪᴍᴇ : {uptime}")
+    await avr.edit(f"-ᴄᴜʀʀᴇɴᴛ ʙᴏᴛ sᴛᴀᴛᴜs-\n\n 🏓 ᴘᴏɴɢ : {time_taken_s:.3f} ms\n\n ⏰ ᴜᴘᴛɪᴍᴇ : {uptime}")
     await asyncio.sleep(DELETE_TIME)
     await avr.delete()
