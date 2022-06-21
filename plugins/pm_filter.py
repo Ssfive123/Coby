e16df851ad3549e3bc670a397ab35c394680b9a6
Shivaprasad <91235488+Ssfive123@users.noreply.github.com>
2022-06-21 09:20:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -39,7 +39,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("🤧 Hey {query.from_user.first_name} This Rquest Not For You, \n\nRquest Your Own 😌", show_alert=True)
+        return await query.answer("🤧 Hey Buddy This Rquest Not For You, \n\nRquest Your Own 😌", show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -88,7 +88,7 @@ async def next_page(bot, query):
     )
     btn.insert(1,
         [
-            InlineKeyboardButton(f'📂 Files: {len(files)}', 'dupe'),
+            InlineKeyboardButton(f'📟 Files: {len(files)}', 'dupe'),
             InlineKeyboardButton(f'🎁 Tips', 'tips'),
             InlineKeyboardButton(f'📮 Info', 'info')
         ]
@@ -700,7 +700,7 @@ async def auto_filter(client, msg, spoll=False):
     )
     btn.insert(1,
         [
-            InlineKeyboardButton(f'📂 Files: {total_results}', 'dupe'),
+            InlineKeyboardButton(f'📟 Files: {total_results}', 'dupe'),
             InlineKeyboardButton(f'🎁 Tips', 'tips'),
             InlineKeyboardButton(f'📮 Info', 'info')
         ]
